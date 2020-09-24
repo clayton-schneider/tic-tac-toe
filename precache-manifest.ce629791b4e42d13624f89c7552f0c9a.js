4e0e740a5bdf2563277b0a4e4e398807fe9980b7
Clayton <schneiderc2016@gmail.com>
2020-09-24 11:50:07 -0400

    Updates

---
+++ precache-manifest.ce629791b4e42d13624f89c7552f0c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bce31b8ff70b7fcac13a9390a08c6a6",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "bcf645a1d09cbee725c1",
+    "url": "/tic-tac-toe/static/css/main.ad27a3fc.chunk.css"
+  },
+  {
+    "revision": "4cb3bac4085a3b0e8829",
+    "url": "/tic-tac-toe/static/js/2.4c40efe2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.4c40efe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf645a1d09cbee725c1",
+    "url": "/tic-tac-toe/static/js/main.e175077a.chunk.js"
+  },
+  {
+    "revision": "822c6dca0d37f87af50e",
+    "url": "/tic-tac-toe/static/js/runtime-main.b8af6e5d.js"
+  }
+]);
